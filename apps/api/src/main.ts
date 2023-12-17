9fc56361ed3adf0f4e705d53177edd59699973b5
Draxx0 <williamfort.lmgl@gmail.com>
2023-12-17 13:29:43 +0100

    add cors, helmet, prefix, global pipe & validateEnv

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { validateEnvVariables } from './validation/server-env.validation';
+import { envs } from './constants/env.constants';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  validateEnvVariables(envs);
+
+  app.setGlobalPrefix('api/v1', { exclude: ['/'] });
+
+  app.use(helmet());
+
+  app.enableCors({
+    //! Should be changed to the production client URL
+    origin: 'http://localhost:3000',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(Number(process.env.PORT) || 8000);
 }
 bootstrap();
