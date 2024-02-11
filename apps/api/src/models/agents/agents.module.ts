73f0d693c0656303ffeadc4149e4f1ee4f600995
Draxx0 <williamfort.lmgl@gmail.com>
2024-02-11 21:16:46 +0100

    :sparkles: Add findAll endpoint to agent

--- apps/api/src/models/agents/agents.module.ts
+++ apps/api/src/models/agents/agents.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AgentsService } from './agents.service';
 import { AgentsController } from './agents.controller';
+import { PaginationModule } from '../common/models/pagination.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AgentEntity } from './entities/agent.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AgentEntity]), PaginationModule],
   controllers: [AgentsController],
   providers: [AgentsService],
 })
--- apps/api/src/models/agents/agents.service.ts
+++ apps/api/src/models/agents/agents.service.ts
@@ -1,15 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAgentDto } from './dto/create-agent.dto';
 import { UpdateAgentDto } from './dto/update-agent.dto';
+import { AgentQueries } from './queries/queries';
+import { PaginationService } from '../common/models/pagination.service';
+import { AgentEntity } from './entities/agent.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginatedData } from '@reqeefy/types';
 
 @Injectable()
 export class AgentsService {
+  constructor(
+    @InjectRepository(AgentEntity)
+    private readonly agentRepository: Repository<AgentEntity>,
+    private readonly paginationService: PaginationService,
+  ) {}
   create(createAgentDto: CreateAgentDto) {
     return 'This action adds a new agent';
   }
 
-  findAll() {
-    return `This action returns all agents`;
+  async findAll(queries: AgentQueries): Promise<PaginatedData<AgentEntity>> {
+    const {
+      page = 1,
+      limit_per_page = 10,
+      search,
+      sort_by = 'created_at',
+      sort_order = 'DESC',
+    } = queries;
+
+    const query = this.agentRepository
+      .createQueryBuilder('agent')
+      .leftJoinAndSelect('agent.user', 'user');
+
+    if (search) {
+      query.where(
+        'agent.user.firstName LIKE :search OR agent.user.lastName LIKE :search',
+        {
+          search: `%${search}%`,
+        },
+      );
+    }
+
+    const [users, total] = await query
+      .skip((page - 1) * limit_per_page)
+      .orderBy(`user.${sort_by}`, sort_order)
+      .take(limit_per_page)
+      .getManyAndCount();
+
+    return this.paginationService.paginate<AgentEntity>({
+      page,
+      total,
+      limit_per_page,
+      data: users,
+    });
   }
 
   findOne(id: number) {
