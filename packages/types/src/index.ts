5cb304bcd62cdd682f2b39310857835a2d46227e
Draxx0 <williamfort.lmgl@gmail.com>
2024-02-12 21:25:17 +0100

    :label: add agency enum activity area types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,4 +1,6 @@
-export type * from './message';export type * from './ticket';
+export type * from './message';
+export type * from './ticket';
 export type * from './user';
 export type * from './project';
 export type * from './pagination';
+export type * from './agency';
