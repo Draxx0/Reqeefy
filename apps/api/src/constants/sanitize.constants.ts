1042d0bb46595c4e76084f2b297da5c09af71909
Draxx0 <williamfort.lmgl@gmail.com>
2024-02-29 20:39:21 +0100

    :sparkles: add sanitizing function & constants

---
+++ apps/api/src/constants/sanitize.constants.ts
@@ -0,0 +1,32 @@
+import sanitizeHtml from 'sanitize-html';
+
+const ALLOWED_TAGS = [
+  'h1',
+  'h2',
+  'img',
+  'a',
+  'p',
+  'ul',
+  'ol',
+  'li',
+  'blockquote',
+  'code',
+  'pre',
+  'strong',
+  'em',
+] as const;
+
+export const SANITIZE_OPTIONS: sanitizeHtml.IOptions = {
+  allowedTags: sanitizeHtml.defaults.allowedTags.concat([...ALLOWED_TAGS]),
+  allowedAttributes: {
+    a: ['href'],
+    img: [
+      'src',
+      {
+        name: 'target',
+        values: ['_blank'],
+      },
+    ],
+  },
+  allowedSchemes: ['http', 'https'],
+};
