cf72e2fef3ada523302f4de22580d1ea437aa8c6
Draxx0 <williamfort.lmgl@gmail.com>
2024-05-04 17:45:55 +0200

    :lock: add midleware for protected route

--- apps/front/middleware.ts
+++ apps/front/middleware.ts
@@ -1,17 +1,61 @@
-import { AUTH_UTILS } from '@/utils/auth/getAuthenticatedUser'; // import { NextResponse } from 'next/server';// import type { NextRequest } from 'next/server';import { PROTECTED_ROUTES } from './constants';
+// import { NextRequest, NextResponse } from 'next/server';
 import { NextRequest, NextResponse } from 'next/server';
 
-// export default function middleware(req: NextRequest) {
-//   const isAuthenticated = AUTH_UTILS.getAuthenticatedUser();
+const publicRoutes = ['/auth/register', '/auth/login'];
+const privateRoutes = ['/tickets', '/distributions', '/user-settings'];
 
-//   if (!isAuthenticated && PROTECTED_ROUTES.includes(req.nextUrl.pathname)) {
-//     const absoluteURL = new URL('/auth/login', req.nextUrl.origin);
-//     return NextResponse.redirect(absoluteURL.toString());
-//   }
-
-//   return NextResponse.next();
-// }
+const superadminRoutes = [
+  '/settings',
+  '/settings/accounts',
+  '/settings/projects',
+];
 
 export default function middleware(req: NextRequest) {
-  return NextResponse.next();
+  const path = req.nextUrl.pathname;
+
+  const isPublicRoute = publicRoutes.includes(path);
+  const isPrivateRoute =
+    privateRoutes.some((route) => path.startsWith(route)) || path === '/';
+  const isSuperadminRoute = superadminRoutes.some((route) =>
+    path.startsWith(route)
+  );
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
+
+  const userCookie = req.cookies.get('USER_DATA')?.value;
+
+  if (!userCookie) {
+    return NextResponse.redirect(new URL('/auth/login', req.url));
+  }
+
+  try {
+    const parsedUser = JSON.parse(userCookie);
+
+    const isValidUser = parsedUser && parsedUser.id && parsedUser.email;
+
+    const isUserSuperadmin = parsedUser.role === 'superadmin';
+
+    if (isValidUser) {
+      if (isPrivateRoute) {
+        return NextResponse.next();
+      }
+      if (isSuperadminRoute && isUserSuperadmin) {
+        return NextResponse.next();
+      }
+    }
+  } catch (error) {
+    console.error("Erreur lors de l'analyse du cookie USER_DATA:", error);
+  }
+
+  return NextResponse.redirect(new URL('/', req.url));
 }
+
+export const config = {
+  matcher: ['/:path((?!api|_next/static|_next/image|favicon\\.ico|assets/).*)'],
+};
+
+// export default function middleware(req: NextRequest) {
+//   return NextResponse.next();
+// }
