df50dab752bb88e26cfcb360637da9f82593736e
Draxx0 <williamfort.lmgl@gmail.com>
2024-05-13 10:57:12 +0200

    :wrench: update  typeormmodule

--- apps/api/src/database/postgres.module.ts
+++ apps/api/src/database/postgres.module.ts
@@ -5,19 +5,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USERNAME'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DATABASE'),
-        entities: [__dirname + '/../models/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      // useFactory: (configService: ConfigService) => ({
+      //   type: 'postgres',
+      //   host: configService.get('POSTGRES_HOST'),
+      //   port: configService.get('POSTGRES_PORT'),
+      //   username: configService.get('POSTGRES_USERNAME'),
+      //   password: configService.get('POSTGRES_PASSWORD'),
+      //   database: configService.get('POSTGRES_DATABASE'),
+      //   entities: [__dirname + '/../models/**/*.entity{.ts,.js}'],
+      //   synchronize: true,
+      // }),
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [__dirname + '/../models/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
   ],
 })
